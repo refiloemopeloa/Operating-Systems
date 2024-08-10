2fe24399f36c9ec37756953fd22b15040fbc6074
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-08-10 18:58:09 +0200

    CLion files

---
+++ Process API/cmake-build-debug/CMakeFiles/arguments.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arguments.dir/arguments.c.o"
+  "CMakeFiles/arguments.dir/arguments.c.o.d"
+  "arguments"
+  "arguments.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/arguments.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Process API/cmake-build-debug/CMakeFiles/cpu.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpu.dir/cpu.c.o"
+  "CMakeFiles/cpu.dir/cpu.c.o.d"
+  "cpu"
+  "cpu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cpu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
