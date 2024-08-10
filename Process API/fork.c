8c82507f6f628a840f150b5dc75554fa5c3e52ac
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-08-10 19:38:37 +0200

    Implemented parent-child check.

--- Process API/fork.c
+++ Process API/fork.c
@@ -5,6 +5,13 @@
 #include <unistd.h>
 int main(){
     int pid = fork();
-    printf("pid = %d\n" , pid);
+
+    if (pid == 0) {
+        printf(" I'm the child %d\n", getpid());
+    }
+    else{
+        printf("My child is called %d\n", pid);
+    }
+    printf("That's it %d\n", getpid());
     return 0;
 }
