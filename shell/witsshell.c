038c0fbdef686970aa5dee22ea5b024e5ebce924
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-08-21 16:36:18 +0200

    Created function for splitting strings with spaces. Implemented while loop which terminates upon typing exit.

--- shell/witsshell.c
+++ shell/witsshell.c
@@ -7,9 +7,39 @@
 #include <assert.h>
 #include <fcntl.h>
 
+#define MAX 32
 
-int main(int MainArgc, char *MainArgv[]){
-	
+void split(char **split_array, char *buffer, int *buffer_size) {
+    *(buffer_size) = 0;
+    while ((*(split_array + *(buffer_size)) = strsep(&buffer, " ")) != NULL) {
+        *(buffer_size) = *(buffer_size) + 1;
+    }
+}
 
-	return(0);
+int main(int MainArgc, char *MainArgv[]) {
+    char *buffer;
+    size_t buffer_size = 0;
+    printf("witsshell> ");
+    size_t character_count = getline(&buffer, &buffer_size, stdin);
+    int split_array_size;
+    char **split_array = (char **) malloc(sizeof(char *) * MAX);
+    split(split_array, buffer, &split_array_size);
+    for (int i = 0; i < split_array_size; i++) {
+        printf("%s\n", split_array[i]);
+    }
+
+    while (strcmp(buffer, "exit\n") != 0) {
+        // printf("You typed: %s", buffer);
+        printf("witsshell> ");
+        split(split_array, buffer, &split_array_size);
+        for (int i = 0; i < split_array_size; i++) {
+            printf("%s\n", split_array[i]);
+        }
+
+        // free(split_array);
+        character_count = getline(&buffer, &buffer_size, stdin);
+    }
+
+    free(split_array);
+    exit(0);
 }
