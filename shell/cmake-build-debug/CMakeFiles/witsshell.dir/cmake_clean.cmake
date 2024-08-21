aed82877f735544f9d2abc743d5515a3ef13661f
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-08-21 16:36:27 +0200

    CMake Files

---
+++ shell/cmake-build-debug/CMakeFiles/witsshell.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/witsshell.dir/witsshell.c.o"
+  "CMakeFiles/witsshell.dir/witsshell.c.o.d"
+  "witsshell"
+  "witsshell.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/witsshell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
