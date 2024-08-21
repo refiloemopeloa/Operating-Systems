c370194685499fea10edba83e9ecc3b6e1352566
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-08-21 14:37:05 +0200

    created zombies.c to demonstrate "zombie" processes, which are processes that can be brought back to life

---
+++ Process API/zombies.c
@@ -0,0 +1,26 @@
+//
+// Created by matth on 2024/08/10.
+//
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+int main(){
+    int pid = fork();
+
+    if (pid == 0) {
+        printf("check the status\n");
+        sleep(10);
+        printf("and again\n");
+        return 42;
+    }
+    else{
+        sleep(20);
+        int res;
+        wait(&res);
+        printf("the result was %d\n", WEXITSTATUS(res));
+        printf("and again\n");
+        sleep(10);
+    }
+    return 0;
+}
