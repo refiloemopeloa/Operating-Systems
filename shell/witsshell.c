df7044313c63fa831794530281a3a6cf4c1fb0d8
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-08-22 14:43:07 +0200

    Implemented echo function through fork() process.

--- shell/witsshell.c
+++ shell/witsshell.c
@@ -17,29 +17,54 @@ void split(char **split_array, char *buffer, int *buffer_size) {
 }
 
 int main(int MainArgc, char *MainArgv[]) {
-    char *buffer;
-    size_t buffer_size = 0;
-    printf("witsshell> ");
-    size_t character_count = getline(&buffer, &buffer_size, stdin);
+    char *buffer; //address of first character of string input
+    size_t buffer_size = 0; //size of input buffer
+
+    //printf("witsshell> ");      //shell input prompt
+
+    //get input string and store in buffer, change size of input buffer
+    size_t character_count;
+    // = getline(&buffer, &buffer_size, stdin);     //number of characters stored in character_count
+
+    //buffer[strcspn(buffer, "\n")] = 0;      // remove '\n' from end of line
     int split_array_size;
     char **split_array = (char **) malloc(sizeof(char *) * MAX);
-    split(split_array, buffer, &split_array_size);
-    for (int i = 0; i < split_array_size; i++) {
-        printf("%s\n", split_array[i]);
-    }
+    int status;
+    //split(split_array, buffer, &split_array_size);
+    //for (int i = 0; i < split_array_size; i++) {
+    //    printf("%s\n", split_array[i]);
+    //}
 
-    while (strcmp(buffer, "exit\n") != 0) {
+    while (1) {
         // printf("You typed: %s", buffer);
         printf("witsshell> ");
+        character_count = getline(&buffer, &buffer_size, stdin);
+        buffer[strcspn(buffer, "\n")] = 0;
+        if (strcmp(buffer, "exit") == 0) {
+            free(split_array);
+            exit(0);
+        }
         split(split_array, buffer, &split_array_size);
-        for (int i = 0; i < split_array_size; i++) {
-            printf("%s\n", split_array[i]);
+        // for (int i = 0; i < split_array_size; i++) {
+        //     printf("%s\n", split_array[i]);
+        // }
+        pid_t p = fork();
+        if (p == 0) {
+            if (strcmp(split_array[0], "echo") == 0) {
+                if (split_array_size < 2) {
+                    printf("\n");
+                } else {
+                    for (int i = 1; i < split_array_size - 1; i++) {
+                        printf("%s ", split_array[i]);
+                    }
+                    printf("%s\n", split_array[split_array_size - 1]);
+                }
+            }
+            exit(0);
+        } else {
+            waitpid(-1, &status, 0);
         }
 
         // free(split_array);
-        character_count = getline(&buffer, &buffer_size, stdin);
     }
-
-    free(split_array);
-    exit(0);
 }
