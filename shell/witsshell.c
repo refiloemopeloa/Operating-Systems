16fdb5d2ae750140cbf1917de4e6e52d7d014004
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-09-12 16:11:37 +0200

    Implemented check for batch mode or iterative mode. Created a function for the shell functionality.

--- shell/witsshell.c
+++ shell/witsshell.c
@@ -8,19 +8,37 @@
 #include <fcntl.h>
 
 #define MAX 32
+char **PATH;
+int path_args = 0;
 
-void split(char **split_array, char *buffer, int *buffer_size) {
+char **split(size_t split_array_size, char *buffer, size_t *buffer_size, char *delimiter) {
     *(buffer_size) = 0;
-    while ((*(split_array + *(buffer_size)) = strsep(&buffer, " ")) != NULL) {
+    char **returned_array = (char **) malloc(sizeof(char *) * split_array_size);
+    while ((*(returned_array + *(buffer_size)) = strsep(&buffer, delimiter)) != NULL) {
         *(buffer_size) = *(buffer_size) + 1;
     }
+    return returned_array;
 }
+
+
+
+void shell(size_t character_count, char *buffer, size_t *buffer_size, char **split_array, int split_array_size,
+           int *status, char *mode) {
+    if (strcmp(mode, "interactive") == 0) {
+        buffer = NULL;
+        character_count = getline(&buffer, buffer_size, stdin);
+        if (character_count == -1) {
+            exit(0);
+        }
+        // character_count = *buffer_size;
+        buffer[strcspn(buffer, "\n")] = 0;
+    }
+}
+
 
 int main(int MainArgc, char *MainArgv[]) {
     char *buffer; //address of first character of string input
     size_t buffer_size = 0; //size of input buffer
-
-    //printf("witsshell> ");      //shell input prompt
 
     //get input string and store in buffer, change size of input buffer
     size_t character_count;
@@ -30,41 +48,18 @@ int main(int MainArgc, char *MainArgv[]) {
     int split_array_size;
     char **split_array = (char **) malloc(sizeof(char *) * MAX);
     int status;
-    //split(split_array, buffer, &split_array_size);
-    //for (int i = 0; i < split_array_size; i++) {
-    //    printf("%s\n", split_array[i]);
-    //}
 
-    while (1) {
-        // printf("You typed: %s", buffer);
-        printf("witsshell> ");
-        character_count = getline(&buffer, &buffer_size, stdin);
-        buffer[strcspn(buffer, "\n")] = 0;
-        if (strcmp(buffer, "exit") == 0) {
-            free(split_array);
-            exit(0);
-        }
-        split(split_array, buffer, &split_array_size);
-        // for (int i = 0; i < split_array_size; i++) {
-        //     printf("%s\n", split_array[i]);
-        // }
-        pid_t p = fork();
-        if (p == 0) {
-            if (strcmp(split_array[0], "echo") == 0) {
-                if (split_array_size < 2) {
-                    printf("\n");
-                } else {
-                    for (int i = 1; i < split_array_size - 1; i++) {
-                        printf("%s ", split_array[i]);
-                    }
-                    printf("%s\n", split_array[split_array_size - 1]);
-                }
-            }
-            exit(0);
-        } else {
-            waitpid(-1, &status, 0);
-        }
 
-        // free(split_array);
+    bool is_batch = false;
+    if (MainArgc > 1) {
+        is_batch = true;
     }
+    if (!is_batch) {
+        //interactive mode
+        while (1) {
+            printf("witsshell> ");
+            shell(character_count, buffer, &buffer_size, split_array, split_array_size, &status, "interactive");
+        }
+    }
+    free(split_array);
 }
